0c54e2dfe7ee7c3c1a478675406b5a76268eba57
Michael <michaelxilinas@gmail.com>
2024-05-30 18:44:34 -0700

    Added friction vibration.

--- prefabs/agent/agent.gd
+++ prefabs/agent/agent.gd
@@ -6,7 +6,7 @@ signal debug_message(message : String)
 # Settings
 
 @export var movement_speed : float = 2500.0
-@export var vibrate := false
+@export var should_vibrate := false
 @export var debug := false
 @export var auto := false
 
@@ -22,12 +22,19 @@ signal debug_message(message : String)
 @export var velocity_reward_strength: float = 0.001
 @export var velocity_reward_debug := false
 
+@export_group("Haptic Feedback Settings")
+@export_range(0, 1, 0.01) var collision_vibration_duration : float = 0.1
+@export_range(0, 1, 0.01) var collision_vibtation_strength : float = 1.0
+
+
 # Member Variables
 
 @onready var agent_controller : AgentController = $AgentController
+@onready var max_velocity = movement_speed / self.linear_damp
 @onready var timer : Timer = $Timer
 var is_colliding := false
 var target_position : Vector2
+var is_vibrating := false
 
 
 # Engine Functions
@@ -40,15 +47,15 @@ func _ready():
 func _input(event):
 	if event is InputEventKey:
 		if event.is_action_pressed("release_mouse"):
-			if vibrate:
-				vibrate = false
+			if should_vibrate:
+				should_vibrate = false
 			else:
-				vibrate = true
+				should_vibrate = true
 
 
 func _physics_process(_delta):
-
 	agent_controller.reward += distance_reward()
+	movement_vibration()
 
 	if auto:
 		var dir_to_target = (target_position - position).limit_length(1.0)
@@ -73,13 +80,13 @@ func _physics_process(_delta):
 func _on_body_entered(body : PhysicsBody2D):
 	is_colliding = true
 
-	if body.name == "Walls":
+	if body.is_in_group("Walls"):
 		agent_controller.reward -= 1
-		if vibrate:
-			Input.start_joy_vibration(0, 0.0, 1.0, 0.1)
+		collision_vibration(collision_vibtation_strength)
 
-	if body.name == "CPU" or body.name == "Player":
+	if body.is_in_group("Agents"):
 		agent_controller.reward += velocity_reward(body)
+		collision_vibration(collision_vibtation_strength)
 
 
 func _on_body_exited(_body):
@@ -103,7 +110,7 @@ func _on_timer_timeout():
 ## Positive when the agent is further than [param max_distance] to its
 ## interaction partner and negative when the agent is closer than
 ## [param min_distance].
-func distance_reward(strength = 0.001):
+func distance_reward(strength = 0.001) -> float:
 	var reward : float = 0.0
 	var distance = global_position.distance_to(interaction_partner.global_position)
 	if distance > max_distance:
@@ -116,20 +123,46 @@ func distance_reward(strength = 0.001):
 	return reward
 
 
-## Apply a reward based on the similarity in velocity of the agents colliding.
-func velocity_reward(rb : RigidBody2D):
+## Apply a reward based on the difference in velocity of two colliding agents.
+func velocity_reward(rb : RigidBody2D) -> float:
 	var diff = abs(rb.linear_velocity.length() - linear_velocity.length()) 
 	var reward = diff * velocity_reward_strength
+	agent_controller.reward += reward
+
 	if velocity_reward_debug:
 		print(reward)
-	agent_controller.reward += reward
-	if vibrate:
-		Input.start_joy_vibration(0, 1.0, 0.0, 0.1)
+
 	return reward
 
 
-## Return a random position inside the given play area.
-func random_position(area : Rect2) -> Vector2: 
+## Return a random position inside the given area.
+func random_position(area: Rect2) -> Vector2: 
 	var random_x = randf_range(area.position.x, area.position.x + area.size.x)
 	var random_y = randf_range(area.position.y, area.position.y + area.size.y)
 	return Vector2(random_x, random_y)
+
+
+## Prevent new vibrations from playing for the given length of time.
+func inhibit_vibrations(duration: float) -> void:
+	is_vibrating = true
+	await get_tree().create_timer(duration).timeout
+	is_vibrating = false
+
+
+## Vibrate the weak motor in the controller while the agent is moving [br].
+## Intensity is controlled by the magnitude of the agent's linear velocity.
+func movement_vibration() -> void:
+	if should_vibrate and not is_vibrating:
+		Input.start_joy_vibration(0,
+			remap(abs(linear_velocity.length()), 0, max_velocity, 0, 1),
+			0, 0)
+
+
+## Play a vibration on collion with another physics object.
+## Uses the strong motor.
+func collision_vibration(strength: float) -> void:
+	if should_vibrate:
+		Input.stop_joy_vibration(0)
+		Input.start_joy_vibration(0, 0.0,
+		strength, collision_vibration_duration)
+		inhibit_vibrations(collision_vibration_duration)
